195a679a6507f7af078ebe8aa1686eb44c1aca9f
leverh leverh <leverh@gmail.com>
2025-03-20 11:40:50 -0400

    Added content

--- src/data/parks.js
+++ src/data/parks.js
@@ -31,29 +31,127 @@ export const parks = [
     personalNotes: "The cave tour was incredible—some passages felt endless! The forest trails and river added a nice contrast to the underground world. Once you get to the park's headquarters, you can book a cave tour. Despite my dislike for crowds, the cave tour was just short enough to not be annoying. The park is also a great place to see bats, especially in the evening."
   },
   {
-    id: "yellowstone",
-    name: "Yellowstone National Park",
-    state: "Wyoming",
-    established: "March 1, 1872",
-    latitude: 44.4280,
-    longitude: -110.5885,
-    thumbnailUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Grand_Prismatic_Spring_2013.jpg/1280px-Grand_Prismatic_Spring_2013.jpg", // Using a placeholder
+    id: "Ashtabula-County",
+    name: "Ashtabula County, Ohio",
+    state: "Ohio",
+    established: "1808",
+    latitude: 41.89,
+    longitude: -80.76,
+    thumbnailUrl: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto:good/v1742242506/20231108_143249_ryf3q0.jpg",
     images: [
       {
-        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Grand_Prismatic_Spring_2013.jpg/1280px-Grand_Prismatic_Spring_2013.jpg",
-        caption: "Grand Prismatic Spring",
-        location: "Midway Geyser Basin"
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742242506/20231108_150524_khxvwz.jpg",
+        caption: "Ashtabula County Covered Bridge",
+        location: "Ashtabula County"
       },
       {
-        url: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7f/Old_Faithful_Yellowstone_close.jpg/1280px-Old_Faithful_Yellowstone_close.jpg",
-        caption: "Old Faithful Eruption",
-        location: "Upper Geyser Basin"
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742242508/20231111_100956_lsazhv.jpg",
+        caption: "Lake Erie shoreline",
+        location: "Ashtabula County"
       },
     ],
-    shortDescription: "America's first national park, known for geothermal features",
-    description: "Yellowstone National Park is a nearly 3,500-sq.-mile wilderness recreation area atop a volcanic hot spot. Mostly in Wyoming, the park spreads into parts of Montana and Idaho too. Yellowstone features dramatic canyons, alpine rivers, lush forests, hot springs and gushing geysers, including its most famous, Old Faithful.",
-    wildlife: ["Bison", "Grizzly Bear", "Wolf", "Elk"],
-    bestTimeToVisit: "May to September",
-    personalNotes: "The geothermal features were unlike anything I've ever seen. The bison traffic jams were a fun experience!"
-  }
+    shortDescription: "Ashtabula County, Ohio, known for its covered bridges and Lake Erie shoreline",
+    description: "The county is best known for having nineteen covered bridges within the county limits, including both the longest and the shortest covered bridges in the United States. The longest is the Smolen–Gulf Bridge at 613 feet long, and the shortest is the West Liberty Street Bridge in Geneva at 18 feet long. The county also has a number of public parks, beaches, and marinas along the Lake Erie shoreline.",
+    wildlife: ["White-tailed Deer", "Bald Eagle", "Great Blue Heron", "Beaver"],
+    bestTimeToVisit: "April to September",
+    personalNotes: "Chilled out area in Ohio. The covered bridges are very cute and make for some nice photos. Lake Erie is beaituful and the beaches are great."
+  },
+  {
+    id: "cape-henlopen",
+    name: "Cape Henlopen State Park",
+    state: "Delaware",
+    established: "1964",
+    latitude: 38.803168,
+    longitude: -75.094626,
+    thumbnailUrl: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto:good/v1742310222/20240501_130521_ndoi8e.jpg",
+    images: [
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742394337/20240501_115150_g4qpmd.jpg",
+        caption: "horseshoe crab",
+        location: "Cape Henlopen State Park"
+      },
+    ],
+    shortDescription: "Cape Henlopen State Park, a coastal park with beaches, dunes, and maritime forests",
+    description: "Cape Henlopen State Park is a coastal park in Delaware. The park has beaches, drunes, and a maritime pine forest.",
+    wildlife: ["Osprey", "White-tailed Deer", "Diamondback Terrapin", "Horseshoe Crab"],
+    bestTimeToVisit: "Spring and fall",
+    personalNotes: "I didn't think it was particularly special or exciting. The forest gave me the worst allergies ever. The most interesting thing were the horseshoe crabs. The observation tower in the middle of the park is also very nice."
+  },
+  {
+    id: "lewes",
+    name: "Lewes, Delaware",
+    state: "Delaware",
+    established: "June 3, 1631",
+    latitude: 38.774444,
+    longitude: -75.139444,
+    thumbnailUrl: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto:good/v1742310212/20240428_160124_lpuljn.jpg",
+    images: [
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742310208/20240428_130629_xvthvw.jpg",
+        caption: "Lewes Beach",
+        location: "Lewes, Delaware"
+      },
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742310204/20240428_104332_mnoh62.jpg",
+        caption: "Lewes Canalfront Park",
+        location: "Lewes, Delaware"
+      },
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742310202/20240428_092048_p7jdna.jpg",
+        caption: "Lewes Sea Front",
+        location: "Lewes, Delaware"
+      }
+    ],
+    shortDescription: "Lewes, Delaware, a historic town with beaches, parks, and museums",
+    description: "Lewes is a historic town in Delaware, known for its beaches, parks, and museums. Visitors can explore the town's charming streets, relax on the sandy beaches, and visit local attractions like the Zwaanendael Museum and the Lewes Historical Society.",
+    wildlife: ["Osprey", "White-tailed Deer", "Diamondback Terrapin", "Horseshoe Crab"],
+    bestTimeToVisit: "Spring, fall, and summer",
+    personalNotes: "Lewes is a cute little town with a lot of history, being the first town in the first state. But Like in the rest of Delaware, there's not much to it other than the beach."
+  },
+  {
+    id: "chicago",
+    name: "Chicago, Illinois",
+    state: "Illinois",
+    established: "1833",
+    latitude: 41.8781,
+    longitude: -87.6298,
+    thumbnailUrl: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto:good/v1742310077/20231117_152352_sfkajy.jpg",
+    images: [
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742310081/20231118_134119_-_Copy_r9gdcz.jpg",
+        caption: "Chicago skyline",
+        location: "Chicago, Illinois"
+      },
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742310074/20231117_145356_hvr4jp.jpg",
+        caption: "Street in North Chicago",
+        location: "Chicago, Illinois"
+      },
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742310072/20231116_160509_lea2yw.jpg",
+        caption: "Unique architecture in Chicago",
+        location: "Chicago, Illinois"
+      },
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742310073/20231116_202718_zdb4si.jpg",
+        caption: "Chicago center",
+        location: "Chicago, Illinois"
+      },
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742310068/20231116_130852_-_Copy_tsigdr.jpg",
+        caption: "The Willis Tower (formerly Sears Tower)",
+        location: "Chicago, Illinois"
+      },
+      {
+        url: "https://res.cloudinary.com/ddsbfxlbq/image/upload/f_auto,q_auto/v1742310067/20231116_125901_oby4d9.jpg",
+        caption: "View from the Sephora mall",
+        location: "Chicago, Illinois"
+      },
+    ],
+    shortDescription: "Chicago, Illinois, a vibrant city with a rich history and diverse culture",
+    description: "Chicago is the 3rd largest city in the United States and is known for its vibrant arts scene, stunning architecture, and diverse neighborhoods. Visitors can explore iconic attractions like Millennium Park, Navy Pier, and the Art Institute of Chicago, as well as enjoy world-class dining, shopping, and entertainment.",
+    wildlife: ["White-tailed Deer", "Bats", "Barred Owl", "Eastern Box Turtle"],
+    bestTimeToVisit: "Spring, fall, and summer",
+    personalNotes: "Chicago was such a nice surprise! I loved the architecture, the vibe, and the weather (it was cold and I love the cold). People are very friendly my travel companion said the deep dish pizza was worth the hype. Definitely a city I will visit again."
+  },
 ]
